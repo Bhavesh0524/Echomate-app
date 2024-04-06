8620886fe477605ff2fab17e2551118936eaddbe
Bhavesh0524 <32071351+Bhavesh0524@users.noreply.github.com>
2024-04-06 11:34:06 +0530

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://13.201.12.205:8800/api/",
+  baseURL: "http://localhost:8800/api/",
   withCredentials: true,
 });
